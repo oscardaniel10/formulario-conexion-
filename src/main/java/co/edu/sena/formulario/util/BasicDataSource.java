fc0d6874df232a0fe14e78db5e69d12075197cb0
oscardaniel10 <125519146+oscardaniel10@users.noreply.github.com>
2023-05-01 22:04:53 -0500

    formulario

---
+++ src/main/java/co/edu/sena/formulario/util/BasicDataSource.java
@@ -0,0 +1,34 @@
+package co.edu.sena.formulario.util;
+
+import java.sql.Connection;
+
+public class BasicDataSource {
+    public void setUrl(String url) {
+    }
+
+    public void setUsername(String user) {
+    }
+
+    public void setPassword(String pass) {
+    }
+
+    public void setInitialSize(int i) {
+    }
+
+    public void setMinIdle(int i) {
+    }
+
+    public void setMaxIdle(int i) {
+    }
+
+    public void setMaxTotal(int i) {
+    }
+
+    public Connection getConnection() {
+        return null;
+    }
+}
+
+
+
+
